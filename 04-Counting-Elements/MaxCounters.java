aba06c95dcd27d0b1a50e987c34bcd7866d75915
Hamid Khan <mhkubaid@gmail.com>
2018-09-09 21:23:17 +0530

    Create MaxCounters.java

---
+++ 04-Counting-Elements/MaxCounters.java
@@ -0,0 +1,33 @@
+// Max Counter: handle N counters based on the elements of an array
+// https://app.codility.com/programmers/lessons/4-counting_elements/max_counters/
+class Solution {
+    public int[] solution(int N, int[] A) {
+        int max = 0;
+        int lastIncrease = 0;
+        int[] counters = new int[N];
+        
+        for(int element : A){
+            if(element > N){
+                lastIncrease = max;
+            } else {
+                if(counters[element-1] < lastIncrease){
+                    counters[element-1] = lastIncrease;
+                }
+                
+                ++counters[element-1];
+                
+                if (max < counters[element-1]) {
+                    max = counters[element-1];
+                }
+            }
+        }
+        
+        for (int i=0; i<N; i++) {
+            if(counters[i] < lastIncrease) {
+                counters[i] = lastIncrease;
+            }
+        }
+        
+        return counters;
+    }
+}
